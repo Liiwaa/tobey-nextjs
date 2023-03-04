b2045d4fbb15a890599245b3daa88b2089738e31
Liiwaa <liwaa.mohtar@gmail.com>
2023-03-04 14:23:17 +0200

    basic structure

---
+++ src/app/employee/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+import GeneralLayout from "@/components/GeneralLayout";
+import PageHead from "@/components/heads/pageHead";
+import Theme from "@/themes/Theme";
+import React from "react";
+
+interface LayoutProps {
+    children: React.ReactNode;
+ }
+
+const EmployeeLayout = ({children}: LayoutProps) => {
+    return (
+        <Theme>
+            <PageHead />
+            <GeneralLayout title="Employee List">
+                {children} 
+            </GeneralLayout>            
+        </Theme>
+    );
+};
+
+export default EmployeeLayout;
