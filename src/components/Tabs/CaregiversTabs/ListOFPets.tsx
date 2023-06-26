813d0685b28f5cd539d6d97efbe29c97175daa88
Liiwaa <liwaa.mohtar@gmail.com>
2023-06-26 19:52:29 +0300

    create first batch of files for the structures

---
+++ src/components/Tabs/CaregiversTabs/ListOFPets.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, NoSsr } from '@mui/material';
+
+
+const CaregiverListOFPetsTab: React.FC = () => {
+  return (
+    <NoSsr>
+        <Box sx={{ marginTop:'15px', marginBottom:'15px' }}>
+          
+        </Box>
+    </NoSsr>
+
+  );
+};
+
+export default CaregiverListOFPetsTab;
