0187b7378454f5b78cca6b1be38df72763c2a278
Liiwaa <liwaa.mohtar@gmail.com>
2023-07-10 21:31:24 +0300

    store token and user - NOT TESTED

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,23 +1,24 @@
-import { LoginAPI, PetshopAPI } from "@/constants/api-routes";
-import { ILogin, LoginDTO } from "@/interfaces/components/Login";
+import { LoginAPI } from "@/constants/api-routes";
+import { ILogin, LoginDTO, LoginModel } from "@/interfaces/components/Login";
 import { mapLoginToLoginDTO } from "@/models/login";
 import { postRequest } from "./genericApi.service";
 import { ServiceResponse } from "@/models/serviceResponse";
+import { handleLoginResponse } from "@/utils/auth";
 
 export const callLogin = async (loginData: ILogin) => {
-    const LoginDataDTO: LoginDTO = mapLoginToLoginDTO(loginData);
-  
-    const response: ServiceResponse<LoginDTO> = new ServiceResponse();
-  
-    try {
-      const result = await postRequest<LoginDTO>(LoginAPI, LoginDataDTO);
-      console.log('result', result);
-      response.payload = result.data;
-    } catch (error: any) {
-      response.error = error.errors;
-      response.message = error.message;
-    }
-  
-    return response;
+  const LoginDataDTO: LoginDTO = mapLoginToLoginDTO(loginData);
+
+  const response: ServiceResponse<LoginModel> = new ServiceResponse();
+
+  try {
+    const result = await postRequest<LoginModel>(LoginAPI, LoginDataDTO);
+    console.log('result', result);
+    handleLoginResponse(result.data); // Pass only the 'data' property to the handleLoginResponse function
+    response.payload = result.data;
+  } catch (error: any) {
+    response.error = error.errors;
+    response.message = error.message;
   }
-  
+
+  return response;
+};
